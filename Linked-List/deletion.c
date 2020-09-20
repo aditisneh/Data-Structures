06daa4b6e5914a69a080f544b3d5559013c71d77
Aditi Sneh <56735634+aditisneh@users.noreply.github.com>
2020-09-20 17:45:24 +0530

    Update deletion.c

--- Linked-List/deletion.c
+++ Linked-List/deletion.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *link;
+};
+ struct node *root;
+ 
+void create(int n);
+void display();
+void delete(int data, int position);
 
+
+int main(){
+	int n;
+	printf("Enter the length of list-");
+	scanf("%d",&n);
+	create(n);
+	printf("Enter the data you want to delete")
+}
+
+void delete(int position){
+	
+}
